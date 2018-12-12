4d9585b327e865964aacbee2666ab0a54f7eda5b
abigail-kaye <agkaye96@gmail.com>
2018-12-12 15:04:43 -0500

    add songList and oneSong page

--- data/songs.js
+++ data/songs.js
@@ -1,5 +1,6 @@
 const mongoCollections = require("../config/mongoCollections");
 const songDb = mongoCollections.songCollection;
+const uuid = require("node-uuid");
 
 let exportedMethods = {
     async getSongByTitle(title) {
@@ -33,6 +34,46 @@ let exportedMethods = {
 
         return false;
     },
+
+    /////////////
+    async searchForSong(searchValue){
+        if (typeof searchValue !== 'string') throw "Please provide a valid search entry";
+
+        let foundSongs = [];
+        const songsCol = await songDb();
+        const songs = await songsCol.find().toArray();
+
+        for (i = 0; i < songs.length; i++) {
+            let song = songs[i];
+            let added = false;
+            if (song.name.contains(searchValue)) {
+                foundSongs.push(song);
+                added = true;
+            }
+            else if (song.artist.contains(searchValue)) {
+                foundSongs.push(song);
+                added = true;
+            }
+            else if (song.album.contains(searchValue)){
+                foundSongs.push(song);
+                added = true;
+            }
+            
+            const songTags = song.tags;
+            
+            for (j = 0; j < songTags.length; j++){
+                let tag = songTags[j];
+                if (tag.contains(searchValue) && added == false){
+                    foundSongs.push(song);
+                    added = true;
+                }
+                    
+            }
+        }
+        return foundSongs;
+    },
+    /////////////
+
     async deleteSong(id) {
         // Error checking
         if (!id) throw "Please provide a valid ID.";
@@ -46,25 +87,29 @@ let exportedMethods = {
             throw `Could not delete song with id of ${id}`;
         }
     },
-    async addSong(title, artist, album, rating) {
+    async addSong(title, artist, album, genre, rating) {
         // Error checking type of input as strings
         if (typeof title !== 'string') throw "Please provide a valid song title.";
         if (typeof artist !== 'string') throw "Please provide a valid artist name.";
         if (typeof album !== 'string') throw "Please provide a valid album title.";
+        if (typeof genre !== 'string') throw "Please provide a valid genre.";
         if (typeof rating !== 'string') throw "Please provide a valid rating.";
 
         // Get song collection, generate id that is only numeric
         const songCollection = await songDb();
-        const generateId = Math.floor(Math.random() * 100000000) + 100;
-        const stringId = generateId.toString();
+        //const generateId = Math.floor(Math.random() * 100000000) + 100;
+        //const stringId = generateId.toString();
 
         // Build song object
         const newSong = {
-            _id: stringId,
+            _id: uuid.v4(),
             title: title,
             artist: artist,
             album: album,
-            rating: rating
+            genre: genre,
+            rating: rating,
+            tags: []
+
         };
 
         // Insert new song and return its submission id 
@@ -72,6 +117,29 @@ let exportedMethods = {
         const newId = newInsertInformation.insertedId;
         return newId;
     },
+
+    ///////////////////
+    async addTag(song, tag1, tag2, tag3, tag4, tag5){
+        if (typeof song !== 'string') throw 'Please provide a valid song title.';
+
+        let tagList = [];
+        tagList = checkTag(tag1);
+        tagList = checkTag(tag2);
+        tagList = checkTag(tag3);
+        tagList = checkTag(tag4);
+        tagList = checkTag(tag5);
+        return tagList;
+    },
+
+    async checkTag(tagList, tag){
+        if (tag !== null){
+            if (typeof tag !== 'string') throw 'Please provide a valid tag';
+            tagList.push(tag);
+        }
+        return tagList;
+    },
+////////////////////////
+
     async updateSong(id, updatedSong) {
         // Error checking
         if (!id) throw "Please provide a valid ID.";
