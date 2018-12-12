a94f4856ac75ae060bb220251c12e8233d9e7285
abigail-kaye <agkaye96@gmail.com>
2018-12-12 14:59:19 -0500

    add songList and oneSong page

---
+++ data/songList.js
@@ -0,0 +1,34 @@
+const mongoCollections = require("../config/mongoCollections");
+const songDb = mongoCollections.songCollection;
+
+let exportedMethods = {
+    async createList(){
+        const songsCol = await songDb();
+        const songs = await songsCol.find().toArray();
+
+        let table = document.getElementById("songTable");
+        
+
+        for (i = 0; i < songs.length; i++){
+            let rowCount = table.rows.length;
+            let row = table.insertRow(rowCount);
+
+            let song = songs[i];
+            row.insertCell(0).innerHTML = song.title;
+            row.insertCell(1).innerHTML = song.artist;
+            row.insertCell(2).innerHTML = song.album;
+            row.insertCell(3).innerHTML = song.genre;
+            row.insertCell(4).innerHTML = song.rating;
+        }
+    },
+
+    /*
+    async deletRow(obj) {
+        var index = obj.parentNode.parentNode.rowIndex;
+        var table = document.getElementById("myTableData");
+        table.deleteRow(index);
+    }
+    */
+
+}
+module.exports = exportedMethods;
